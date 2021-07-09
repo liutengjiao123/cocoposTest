5078baf01e873c13f7599011c1224510d15855da
liu <1425110265@qq.com>
2021-07-09 14:04:52 +0800

    首版本提交

---
+++ Classes/pickerTool/PickerTool.h
@@ -0,0 +1,17 @@
+//
+//  PickerTool.h
+//  ios_cocopodsTest
+//
+//  Created by liutengjiao on 2021/7/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PickerTool : NSObject
++(void)log;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/pickerTool/PickerTool.m
@@ -0,0 +1,14 @@
+//
+//  PickerTool.m
+//  ios_cocopodsTest
+//
+//  Created by liutengjiao on 2021/7/9.
+//
+
+#import "PickerTool.h"
+
+@implementation PickerTool
++(void)log{
+    NSLog(@"123456");
+}
+@end
